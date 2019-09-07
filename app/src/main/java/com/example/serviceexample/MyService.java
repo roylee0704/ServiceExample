6cc7116badb0350e23a576a52409d5d1c523ca10
roylee0704 <roylee0704@gmail.com>
2019-09-07 14:42:09 +0700

    Integrate AWS-SQS

--- app/src/main/java/com/example/serviceexample/MyService.java
+++ app/src/main/java/com/example/serviceexample/MyService.java
@@ -6,7 +6,17 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.provider.Settings;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.handlers.AsyncHandler;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+
 import androidx.annotation.Nullable;
+
+=
 
 public class MyService extends Service {
     private MediaPlayer player;
@@ -31,5 +41,26 @@ public class MyService extends Service {
         super.onDestroy();
 
         player.stop();
+    }
+
+    public void addItemSQS() {
+       // Timber.i( "Pull: addItemSQS: ");
+
+        AWSCredentials awsCredentials=new BasicAWSCredentials("ACCESS_KEY" ,"SECRET_KEY");
+
+        AmazonSQSAsyncClient sqs = new AmazonSQSAsyncClient(awsCredentials);
+        sqs.setRegion(Region.getRegion("ap-southeast-1"));
+        SendMessageRequest req = new SendMessageRequest("https://sqs.ap-southeast-1.amazonaws.com/ACCOUNT_ID/QUEUE_NAME", "hello world");
+        sqs.sendMessageAsync(req, new AsyncHandler<SendMessageRequest, SendMessageResult>() {
+            @Override
+            public void onSuccess(SendMessageRequest request, SendMessageResult sendMessageResult) {
+                //Timber.i( "Pull: SQS result: " + sendMessageResult.getMessageId());
+            }
+
+            @Override
+            public void onError(Exception e) {
+             //   Timber.e( "Pull: SQS error: ", e);
+            }
+        });
     }
 }
